be72139052bd78897c96ea4cc438de24ce11cdfa
Indrasen23 <126501057+Indrasen23@users.noreply.github.com>
2024-04-02 00:21:31 +0530

    user update api route created

--- api/index.js
+++ api/index.js
@@ -1,20 +1,26 @@
-import express from 'express'
+import express from 'express';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 
+
 dotenv.config();
 const app = express();
-app.use(express.json());
 
 
 
 mongoose.connect(process.env.MONGODB_ATLAS).then(() => {
     console.log('Connected to MongoDB');
-}).catch ((err) => {
+}).catch((err) => {
     console.log(err);
 })
+
+
+
+app.use(express.json());
+app.use(cookieParser());
 
 
 
