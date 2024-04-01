6516735510e3e07b3be374eb5a9d6086244662f7
Indrasen23 <126501057+Indrasen23@users.noreply.github.com>
2024-04-01 16:52:47 +0530

    complete sign in page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -25,7 +25,7 @@ export const signin = async (req, res, next) => {
         if (!validUser) return next(errorHandler(404, 'User not found!'));
 
         const validPassword = bcryptjs.compareSync(password, validUser.password);
-        if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
+        if (!validPassword) return next(errorHandler(401, 'Incorrect password!'));
 
         // console.log('JWT_SECRET:', process.env.JWT_SECRET);
         const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import { React, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
 
 const SignIn = () => {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();  // prevent refresh of the page 
+    try {
+      setLoading(true);
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+
+      const data = await res.json();
+      console.log(data);
+
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+
+      setLoading(false);
+      setError(null);
+      navigate('/');
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+
   return (
-    <div className='text-white'>
-      Sign In
+    <div className='bg-zinc-800   mx-auto  w-[400px] md:w-[600px] border border-gray-400 my-8 pb-8 rounded-lg shadow-sm'>
+
+      <h1 className='text-white text-3xl text-center font-semibold my-7'>Sign In</h1>
+
+      <form onSubmit={handleSubmit} className='flex flex-col gap-6 '>
+        <input type='email' placeholder='indrasen@gmail.com' id='email' onChange={handleChange} autoComplete='current-email' className='border bg-white p-3 rounded-lg w-[85%] mx-auto' />
+        <input type='password' placeholder='*****' id='password' onChange={handleChange} autoComplete='current-password' className='border bg-white p-3 rounded-lg w-[85%] mx-auto' />
+        <button disabled={loading} className='bg-violet-500 text-white font-bold p-3 w-[85%] mx-auto rounded-lg uppercase hover:opacity-90 disabled:opacity-85'>{loading ? 'Loading...' : 'Sign In'}</button>
+      </form>
+
+      <div className=' pl-11 my-[15px] flex gap-3'>
+        <p className='text-white'>Do not have an account?</p>
+        <Link to="/sign-up">
+          <span className='text-violet-200 hover:underline '>Sign Up</span>
+        </Link>
+      </div>
+      {error && <p className='text-red-500 mt-5 ml-11'>{error}</p>}
     </div>
   )
 }
