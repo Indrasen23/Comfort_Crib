9beb64c8504b0ed80ef55865350d76f8bda36a82
Indrasen23 <126501057+Indrasen23@users.noreply.github.com>
2024-04-04 22:18:20 +0530

    create search api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -63,5 +63,67 @@ export const getListing = async (req, res, next) => {
     } catch (error) {
         next(error);
     }
+};
 
-}
+
+
+export const getListings = async (req, res, next) => {
+    try {
+        const limit = parseInt(req.query.limit) || 9; // show a max of 9 listing if no such limit is provided
+        const startIndex = parseInt(req.query.startIndex) || 0;
+
+        let type = req.query.type;
+        if (type === undefined || type === 'all') {
+            type = { $in: ['sale', 'rent'] };
+        }
+
+
+        let furnished = req.query.furnished;
+        if (furnished === undefined || furnished === 'false') {
+            furnished = { $in: [false, true] };
+        }
+
+        let parking = req.query.parking;
+        if (parking === undefined || parking === 'false') {
+            parking = { $in: [false, true] };
+        }
+
+        let wifi = req.query.wifi;
+        if (wifi === undefined || wifi === 'false') {
+            wifi = { $in: [false, true] };
+        }
+
+        let privateBathroom = req.query.privateBathroom;
+        if (privateBathroom === undefined || privateBathroom === 'false') {
+            privateBathroom = { $in: [false, true] };
+        }
+
+        let cctv = req.query.cctv;
+        if (cctv === undefined || cctv === 'false') {
+            cctv = { $in: [false, true] };
+        }
+
+        const searchTerm = req.query.searchTerm || '';
+
+        const sort = req.query.sort || 'createdAt';
+
+        const order = req.query.order || 'desc';
+
+        const listings = await Listing.find({
+            name: { $regex: searchTerm, $options: 'i' },
+            type,
+            furnished,
+            parking,
+            wifi,
+            privateBathroom,
+            cctv,
+        })
+            .sort({ [sort]: order })
+            .limit(limit)
+            .skip(startIndex);
+
+        return res.status(200).json(listings);
+    } catch (error) {
+        next(error);
+    }
+};
